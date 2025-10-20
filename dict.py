9c6fe59f187da4b93ebf36f86e0f13261e4433cd
sachinaneboinwad87-ship-it <sachinaneboinwad87@gmail.com>
2025-10-20 23:40:17 +0530

    Dictionary

---
+++ dict.py
@@ -0,0 +1,32 @@
+# Create and print a dictionary:
+
+dict1 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(dict1)
+#Duplicate values will overwrite existing values:
+
+dict1 = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(dict1)
+
+#Using the dict() method to make a dictionary:
+
+dict2 = dict(name = "John", age = 36, country = "Norway")
+print(dict)
+
+#String, int, boolean, and list data types:
+
+dict3 = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+print(dict3)
